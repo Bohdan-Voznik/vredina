dc8f24afbf80c2da50a8fb0aa4933a54c216288d
Frontend-Bohdan-V <106681988+Frontend-Bohdan-V@users.noreply.github.com>
2024-01-13 01:26:34 +0200

    feat: first commit

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,5 @@
 import { Header } from '../Header';
 import { Footer } from '../Footer';
-import { Container } from '../Container/Container';
 import { LayoutStyled } from './Layout.styled';
 
 export const Layout = ({ children }) => {
